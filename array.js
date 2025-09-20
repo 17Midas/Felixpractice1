671c52a17ac9b067162b9d8ad105012d6c9bb179
felixo <mysticmarsh2000@gmail.com>
2025-09-21 00:15:43 +0300

    additional knowledge on arrays and object

---
+++ array.js
@@ -0,0 +1,7 @@
+let favTeam = ['madrid','united','al nassr'];
+
+favTeam[3]='breaking bad';
+favTeam[4] ='terminal';
+favTeam[0]='real madrid';
+console.log(favTeam[1])
+ 
---
+++ object.js
@@ -0,0 +1,15 @@
+let sister ={
+name:'faith', age:22, eyeColor:'brown'
+}
+console.log(sister)
+
+let brother={
+    name:'robin', age:12, eyeColor:'sky',favTeam:'city'
+}
+console.log(brother)
+if (age<15);{
+    console.log('is my younger sibling')
+}
+if (age>15) {
+    console.log('you are my little old sibling')
+}
